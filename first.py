f56dde9abe2e40f66b912fe3ee047ae29e2fca37
Tnihal94 <tnihal94@gmail.com>
2025-07-27 17:03:18 +0530

    first.py

---
+++ first.py
@@ -0,0 +1,25 @@
+#print("Hello World")
+# This is how we add a comment in python
+
+# This a variable
+a=5
+print(a)
+
+# This a variable
+Instructor = 'Mr. Nihal'
+print("Pyhton by",Instructor, sep='- -')
+
+a=5
+b=5
+print(a+b)
+
+# python as a calculator
+print("5+3")
+print(5+3)
+
+b=10
+c=20
+print(b+c)
+print(c/b)
+print(c*b)
+print(c%b)
