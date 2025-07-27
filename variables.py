7e0a95a0eb8290d20255ee50a0757b21137fd1b9
Tnihal94 <tnihal94@gmail.com>
2025-07-27 17:26:27 +0530

    variables.py

---
+++ variables.py
@@ -0,0 +1,75 @@
+# variables in python
+a = 1
+print(a)
+
+b = 1.1357101
+print(b)
+
+c = "Nihal"
+print(c)
+
+Nihal=26
+d = Nihal
+print(d)
+
+x = 10
+print(x+1)
+
+p,q,r = 11,12,13
+print(p,q,r)
+
+a = 1,2,3,4,5,6,7,8,9,10
+print(a)
+
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+h=8
+i=9
+j=10
+print(a,b,c,d,e,f,g,h,i,j)
+
+MyName = "Nihal"
+myName = "Nihal"
+myname = "Nihal"
+my_name = "Nihal"
+print(MyName)       # pascal case
+print(myName)       # camel case
+print(myname)       # flat case
+print(my_name)      # snake case
+
+# Rules to declare a Variables
+# # 1. Variable name must be start with:
+_my_name = "Tekchand Nihal"
+print(_my_name) 
+
+
+# # 2. Variable name can contain: letters, numbers, underscore (_)
+my_name = "Nihal"
+r1 = 26
+_1 = 1
+
+# # 3. Variables are case sensitive
+myname = "Tekchand"
+print(myName)      # can't run this code
+
+# sale_per_year
+
+# Sales_per_year
+
+# # 4.Variables names can't be a reserved word
+#for = 1
+ #   print(for) # for is a reserved word in python
+
+
+
+
+
+
+
+
+
